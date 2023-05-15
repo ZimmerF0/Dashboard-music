68ab938e6e7ea3ff30b53d138a85a5eea0761ff5
Kishkin Vitalii <dimpla30@gmail.com>
2023-05-15 11:05:17 +0300

    Daily Views Card

--- src/components/Metrics.jsx
+++ src/components/Metrics.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar";
+import DailyMetric from "./DailyMetric";
 
 const Metrics = () => {
   return (
     <div className="metrics">
       <Navbar />
-      <div className="grid__one"></div>
+      <div className="grid__one">
+        <DailyMetric />
+      </div>
       <div className="grid__two"></div>
     </div>
   );
